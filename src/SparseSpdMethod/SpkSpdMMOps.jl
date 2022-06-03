b2452b815e3caaa0c84901a8643217c7cc80b61d
Petr Krysl <pkrysl@ucsd.edu>
2022-06-02 17:04:52 -0700

    removed doc strings for private functions

--- src/SparseSpdMethod/SpkSpdMMOps.jl
+++ src/SparseSpdMethod/SpkSpdMMOps.jl
@@ -13,62 +13,50 @@ using Libdl
 # import LinearAlgebra, OpenBLAS64_jll
 # LinearAlgebra.BLAS.lbt_forward(OpenBLAS64_jll.libopenblas_path)
 
-"""
-    assmb(tlen::IT, nj::IT, temp::Matrix{FT}, relcol::Vector{IT}, relind::Vector{IT}, xlnz::Vector{IT}, lnz::Vector{FT}, jlen::IT) where {IT, FT}
+# This routine performs an indexed assembly (i.e., scatter - add)
+# operation, assuming data structures used in some of our sparse
+# lu codes.
 
-This routine performs an indexed assembly (i.e., scatter - add)
-operation, assuming data structures used in some of our sparse
-lu codes.
+# input parameters:
+# tlen - number of rows in temp.
+# nj - number of columns in temp.
+# temp - block update to be incorporated into factor storage.
+# relcol - relative column indices.
+# relind - relative indices for mapping the updates onto the target columns.
+# xlnz - pointers to the start of each column in the target matrix.
+# jlen - the length the matrix to be updated
 
-input parameters:
-tlen - number of rows in temp.
-nj - number of columns in temp.
-temp - block update to be incorporated into factor storage.
-relcol - relative column indices.
-relind - relative indices for mapping the updates onto the target columns.
-xlnz - pointers to the start of each column in the target matrix.
-jlen - the length the matrix to be updated
-
-output parameters:
-lnz - contains columns modified by the update matrix.
-"""
+# output parameters:
+# lnz - contains columns modified by the update matrix.
 function assmb(tlen::IT, nj::IT, temp::Vector{FT}, relcol::SubArray{IT, 1, Vector{IT}, Tuple{UnitRange{IT}}, true}, relind::SubArray{IT, 1, Vector{IT}, Tuple{UnitRange{IT}}, true}, xlnz::SubArray{IT, 1, Vector{IT}, Tuple{UnitRange{IT}}, true}, lnz::Vector{FT}, jlen::IT) where {IT, FT}
-    # relindk = relind[1:tlen]
     for j in 1:nj
         lbot = xlnz[jlen - relcol[j] + 1] - 1
-        # lnz[lbot .- relindk] += temp[(j-1)*tlen + 1:j*tlen]
         @inbounds for k in 1:tlen
-            # lnz[lbot - relindk[k]] += temp[(j-1)*tlen + k]
             lnz[lbot - relind[k]] += temp[(j-1)*tlen + k]
         end
     end
     return true
 end
 
-"""
-    ldindx(jlen::IT, lindx::Vector{IT}, indmap::Vector{IT}) where {IT}
+# This routine computes the second index vector used to implement the
+# doubly-indirect saxpy-like loops that allow us to accumulate update columns
+# directly into factor storage.
 
-This routine computes the second index vector used to implement the
-doubly-indirect saxpy-like loops that allow us to accumulate update columns
-directly into factor storage.
+#      input parameters -
+#         jlen - length of the first column of the supernode,
+#                  including the diagonal entry.
+#         lindx - the off - diagonal row indices of the supernode,
+#                  i.e., the row indices of the nonzero entries
+#                  lying below the diagonal entry of the first
+#                  column of the supernode.
 
-     input parameters -
-        jlen - length of the first column of the supernode,
-                 including the diagonal entry.
-        lindx - the off - diagonal row indices of the supernode,
-                 i.e., the row indices of the nonzero entries
-                 lying below the diagonal entry of the first
-                 column of the supernode.
-
-     output parameters -
-        indmap - this index vector maps every global row index
-                 of nonzero entries in the first column of the
-                 supernode to its position in the index list
-                 relative to the last index in the list.  more
-                 precisely, it gives the distance of each index
-                 from the last index in the list.
-
-"""
+#      output parameters -
+#         indmap - this index vector maps every global row index
+#                  of nonzero entries in the first column of the
+#                  supernode to its position in the index list
+#                  relative to the last index in the list.  more
+#                  precisely, it gives the distance of each index
+#                  from the last index in the list.
 function ldindx(jlen::IT, lindx::SubArray{IT, 1, Vector{IT}, Tuple{UnitRange{IT}}, true}, indmap::Vector{IT}) where {IT}
     # indmap[lindx[1:jlen]] .= (jlen - 1):-1:0
 
@@ -81,13 +69,7 @@ function ldindx(jlen::IT, lindx::SubArray{IT, 1, Vector{IT}, Tuple{UnitRange{IT}
     return true
 end
 
-# *
-# *
-#         igathr .... integer gather operation      ^^^^^^^
-# *
-# *
-#
-#     purpose - this routine performs a standard integer gather
+# this routine performs a standard integer gather
 #               operation.
 #
 #     input parameters -
@@ -110,31 +92,27 @@ function igathr(klen::IT, lindx::SubArray{IT, 1, Vector{IT}, Tuple{UnitRange{IT}
     end
 end
 
-"""
-   purpose -
-       this routine performs a matrix-matrix multiply, z = z + xy,
-       assuming data structures used in some of our sparse cholesky
-       codes.
+#     this routine performs a matrix-matrix multiply, z = z + xy,
+#     assuming data structures used in some of our sparse cholesky
+#     codes.
+#
+#     matrix x has only 1 column and matrix y has only 1 row. QUESTION: Is this a rank-one update?
+# 
+# input parameters -
+#     m       -   number of rows in x and in z.
+#     q       -   number of columns in y and z.
+#     zindxr  -   zindxr gives the list of rows in z.
+#     zindxc  -   zindxc gives the list of columns in z.
+#     x       -   contains the rows of x.
+#     y(*)    -   contains the columns of y.
+#     iz(*)   -   iz(col) points to the beginning of column col.
+#     relind  -   where each subscript should go in z relative to the 
+#                 bottom
+#     diag    -   the diagonal elements of the column X.  This 
+#                 option parameter allows for scaling of the column.
 
-       matrix x has only 1 column and matrix y has only 1 row. QUESTION: Is this a rank-one update?
-
-   input parameters -
-       m       -   number of rows in x and in z.
-       q       -   number of columns in y and z.
-       zindxr  -   zindxr gives the list of rows in z.
-       zindxc  -   zindxc gives the list of columns in z.
-       x       -   contains the rows of x.
-       y(*)    -   contains the columns of y.
-       iz(*)   -   iz(col) points to the beginning of column col.
-       relind  -   where each subscript should go in z relative to the 
-                   bottom
-       diag    -   the diagonal elements of the column X.  This 
-                   option parameter allows for scaling of the column.
-
-   updated parameters -
-       z       -   on output, z = z + xy.
-
-"""
+# updated parameters -
+#     z       -   on output, z = z + xy.
 function mmpyi(m::IT, q::IT, 
     zindxr::SubArray{IT, 1, Vector{IT}, Tuple{UnitRange{IT}}, true}, 
     zindxc::SubArray{IT, 1, Vector{IT}, Tuple{UnitRange{IT}}, true}, 
@@ -163,25 +141,21 @@ function vswap(m, va, vb)
     end
 end
 
-"""
-   
-This routine performs row swapping inside the Upper block
-supernodes of the Cholesky factorization.  This operation is
-done to match to the row swapping performed on the diagonal
-blocks of L.
+# This routine performs row swapping inside the Upper block
+# supernodes of the Cholesky factorization.  This operation is
+# done to match to the row swapping performed on the diagonal
+# blocks of L.
 
-   input parameters -
-       m - number of rows in the supernode.
-       n - number of columns in the supernode.
-       a - the matrix to have its rows pivoted.
-       lda - the real length of supernode a.
-       ipvt(*) - contains the pivotal information.
+#    input parameters -
+#        m - number of rows in the supernode.
+#        n - number of columns in the supernode.
+#        a - the matrix to have its rows pivoted.
+#        lda - the real length of supernode a.
+#        ipvt(*) - contains the pivotal information.
 
-   updated parameters -
-       a - on output, a has its rows swapped according to
-                   a[i, k] = a[ipvt[i], k] (i hope)
-
-"""
+#    updated parameters -
+#        a - on output, a has its rows swapped according to
+#                    a[i, k] = a[ipvt[i], k] (i hope)
 function luswap(m::IT, n::IT, a::SubArray{FT, 1, Vector{FT}, Tuple{UnitRange{IT}}, true}, lda::IT, ipvt::SubArray{IT, 1, Vector{IT}, Tuple{UnitRange{IT}}, true}) where {IT, FT}
     for k in 1:n
         i = ipvt[k]
