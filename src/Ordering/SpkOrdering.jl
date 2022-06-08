5884abfdee6eb5086f93cefef030151237378dff
Petr Krysl <pkrysl@ucsd.edu>
2022-06-08 06:16:15 -0700

    improve ordering docs

--- src/Ordering/SpkOrdering.jl
+++ src/Ordering/SpkOrdering.jl
@@ -33,6 +33,35 @@ module SpkOrdering
     Ordering{IT}
 
 Type of ordering of the rows and columns.
+
+- `nrows` is the number of rows in the matrix
+- `ncols` is the number of columns in the matrix
+
+Ordering objects contain two permutations and their inverses:
+
+- `rperm` is a row permutation: `rperm[i] = k` means that the new position of
+    row `k` is in position `i` in the new permutation.
+- `rinvp` is a row permutation satisfying `rinvp[rperm[i]] = i`. Thus, `rinvp[k]`
+    provides the position in the new ordering of the original row `k`.
+
+`cperm` and `cinvp` are analogous to `rperm` and `rinvp`, except they apply
+    to column permutations of the matrix.
+
+When the matrix is symmetrically permuted, `rperm` = `cperm` and `rinvp` =
+`cinvp`.
+
+- `xrowblk`  is an array that is sometimes used to contain a partitioning
+    of the rows of the matrix, or both the rows and columns when the
+    matrix is symmetric.
+
+- `nrowblks` is the number of blocks in the partitioning; the rows of the
+    i - th partition are `xrowblk[i]`, `xrowblk[i] + 1` `...` `xrowblk[i + 1] - 1`.
+    For convenience, `xrowblk` has `nrowblks + 1` elements, with
+    `xrowblk[nrowblks + 1]` `= nrows + 1`.
+
+When the matrix is not symmetric, and a partitioning of the columns is required
+as well, the pair `(ncolblks, xcolblk)` is used.
+
 """
 mutable struct Ordering{IT}
     nrows::IT
@@ -57,9 +86,8 @@ row ordering and column ordering are the same. That is, that the matrix is
 square.
 
 Input Parameters:
-  order - the ordering (declared in the calling program)
-  nRows - the number of rows (and columns) in the matrix
-  objectName - the name of the ordering object (optional)
+- `nrows` - the number of rows (and columns) in the matrix
+  
 """
 function Ordering(nrows::IT) where {IT}
     return Ordering(nrows, nrows)
@@ -70,13 +98,11 @@ end
 
 Construct an ordering object. 
 
-The arrays `rperm`, `cperm`, `rinvp`, `cinvp` are allocated and initialized to the
-identity permutation.
+The arrays `rperm`, `cperm`, `rinvp`, `cinvp` are allocated and initialized to
+the identity permutation.
 
 Input Parameter:
-  order - the ordering (declared in the calling program)
-  nRows, nCols - the number of rows and columns in the matrix
-  objectName - the name of the ordering object (optional)
+- `nrows`, `ncols` - the number of rows and columns in the matrix
 """
 function Ordering(nrows::IT, ncols::IT) where {IT}
     #       The default is to set nRowBlks and nColBlks to 0.
