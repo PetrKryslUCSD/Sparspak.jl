2c376e0c286d01ec587a75ab6d9943883b16d6cf
Petr Krysl <pkrysl@ucsd.edu>
2023-03-28 07:35:46 -0700

    use solve! in ldiv!

--- src/SparseCSCInterface/SparseCSCInterface.jl
+++ src/SparseCSCInterface/SparseCSCInterface.jl
@@ -253,26 +253,27 @@ function sparspaklu!(lu::SparseSolver{IT,FT}, m::SparseMatrixCSC{FT,IT}) where {
     lu
 end
 
-
 """
-    ldiv(u,lu::SparseSolver,v)
+    ldiv!(u, lu::SparseSolver{IT,FT}, v) where {IT,FT}
 
-Left division for SparseSolver
+Left division for SparseSolver.
+
+The solution is returned in `u`. The right hand side vector is `v`.
 """
 function LinearAlgebra.ldiv!(u, lu::SparseSolver{IT,FT}, v) where {IT,FT}
     u.=v
-    triangularsolve!(lu,u) || ErrorException("Triangular Solve.")
-    lu._trisolvedone = false
-    u
+    return ldiv!(lu, u)
 end
 
 """
-    ldiv(lu::SparseSolver,v)
+    ldiv!(lu::SparseSolver{IT,FT}, v) where {IT,FT}
 
 Overwriting left division for SparseSolver.
+
+The solution is returned in `v`, which is also the right hand side vector.
 """
 function LinearAlgebra.ldiv!(lu::SparseSolver{IT,FT}, v) where {IT,FT}
-    triangularsolve!(lu,v) || ErrorException("Triangular Solve.")
+    solve!(lu, v) || ErrorException("Triangular Solve.")
     lu._trisolvedone = false
     v
 end
