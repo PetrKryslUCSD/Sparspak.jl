84378239436b27199dfad301fe2ae5b22f02fb84
Petr Krysl <pkrysl@ucsd.edu>
2025-08-01 17:42:22 -0700

    add a solve test

--- test/test_spd_sparse_method.jl
+++ test/test_spd_sparse_method.jl
@@ -84,6 +84,38 @@ _test()
 end # module
 
 
+module mspdsprs003
+using Test
+using LinearAlgebra
+using SparseArrays
+using Sparspak.SpkProblem
+using Sparspak.SpkProblem: outsparse
+using Sparspak.SpkSparseSpdSolver: SpkSparseSpdSolver, findorder!, symbolicfactor!, inmatrix!, solve!
+
+function _test()
+    # Matrix from Figure 3.1.3
+    M, N = 6, 6
+    I = [1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 6]
+    J = [1, 2, 6, 1, 2, 3, 4, 2, 3, 5, 2, 4, 3, 5, 6, 1, 5, 6]
+    V = [1.0 for _ in I]
+    spm = sparse(I, J, V, M, N)
+    p = SpkProblem.Problem(M, N)
+    SpkProblem.insparse!(p, spm)
+    s = SpkSparseSpdSolver.SparseSpdSolver(p)
+    findorder!(s)
+    symbolicfactor!(s)
+    inmatrix!(s)
+    solve!(s)
+    @show p
+    A = Float64.(outsparse(p)) # no generic method for sparse...
+    x = A \ p.rhs
+    @test norm(p.x - x) / norm(x) < 1.0e-6
+    return true
+end
+
+_test()
+end # module
+
 # module mspdsprs004
 # using Test
 # using LinearAlgebra
